01f1857dc437e5dc20feb92da7d3e69ad99499b1
ElhamFadel <elham2000fadel@gmail.com>
2026-01-11 16:33:43 +0200

    Inject queryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import "./index.css";
 import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
 import { ProductsModule } from "./modules/Products/index.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const { Provider: ProductsProvider } = ProductsModule();
-
+const queryClient = new QueryClient({
+  defaultOptions: {queries:{ refetchOnWindowFocus: false, retry: false }}
+})
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
+      <QueryClientProvider client={queryClient}>
       <ProductsProvider>
         <App />
       </ProductsProvider>
+      </QueryClientProvider>
     </MantineProvider>
   </StrictMode>
 );
