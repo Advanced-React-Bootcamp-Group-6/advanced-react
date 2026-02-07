3e4c9e786984a5ac06d813191d3920ef923b1e01
Abdallah Abusharekh <134795680+abdallah-abusharekh@users.noreply.github.com>
2026-02-07 19:44:13 +0200

    chore(design-system): add color/spacing/text types and align css variables (#37)

---
+++ packages/ui/src/types/Color.ts.ts
@@ -0,0 +1,12 @@
+export type Color =
+  | "initial"
+  | "brand-primary"
+  | "brand-secondary"
+  | "success"
+  | "error"
+  | "warning"
+  | "info"
+  | "text-primary"
+  | "text-muted"
+  | "border"
+  | "white";
---
+++ packages/ui/src/types/Text.ts
@@ -0,0 +1,6 @@
+export type Paragraph = "regular" | "semibold";
+
+export type Span = "small-regular" | "regular" | "large-semibold";
+
+export type HeadingLevel = "h1" | "h2";
+export type HeadingWeight = "regular" | "bold";
